8199f844e0ca6f45b196055be878a3a4046f9d0a
Silas Gon <silasidc@gmail.com>
2021-02-24 23:45:13 -0300

    concluindo tabela user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,6 +9,14 @@ class UserController {
 
         const usersRepository = getRepository(User);
 
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            return response.status(400).json({ error: "User already exists!" });
+        }
+
         const user = usersRepository.create({
             name, email
         });
